eb53a827c821adb59df641ea336143093bd4c41f
Christopher <cgortizm21@gmail.com>
2022-06-06 15:14:27 -0500

    style(client): Create the Header component

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+const Layout = ({ children }) => {
+  return (
+    <React.Fragment>
+      <Header />
+      {children}
+      <h2>footer</h2>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
