61a46fb7923944f0094e500deef00ed8c84097bb
Christopher <cgortizm21@gmail.com>
2022-06-06 15:20:27 -0500

    style(client): add some meta tags in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -5,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
