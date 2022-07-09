afa2aa0b118999876a23602cd3d26e5287ae7cf8
Christopher <cgortizm21@gmail.com>
2022-07-09 18:35:50 -0500

    refactor(client): put the styles into a folder in static

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'nprogress/nprogress.css';
-import '../static/styles.css';
+import '../static/css/styles.css';
 
 function MyApp({ Component, pageProps }) {
   return (
