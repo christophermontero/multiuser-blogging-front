42dfed210ef2f49c0096162627adcb9f75fe4567
Christopher <cgortizm21@gmail.com>
2022-07-13 12:59:38 -0500

    feat(client): add the Editor component

--- components/CreateBlog.js
+++ components/CreateBlog.js
@@ -1,9 +1,27 @@
-const CreateBlog = () => {
+import { withRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Editor from './Editor';
+
+const CreateBlog = ({ router }) => {
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    setEditorLoaded(true);
+  }, []);
+
   return (
-    <>
-      <div>Create Blog form</div>
-    </>
+    <div className="my-2">
+      {JSON.stringify(router)}
+      <Editor
+        name="description"
+        onChange={(data) => {
+          setData(data);
+        }}
+        editorLoaded={editorLoaded}
+      />
+    </div>
   );
 };
 
-export default CreateBlog;
+export default withRouter(CreateBlog);
--- pages/admin/blog.js
+++ pages/admin/blog.js
@@ -1,6 +1,9 @@
 import Admin from '../../components/auth/Admin';
-import CreateBlog from '../../components/CreateBlog';
 import Layout from '../../components/Layout';
+import dynamic from 'next/dynamic';
+const CreateBlog = dynamic(() => import('../../components/CreateBlog'), {
+  ssr: false
+});
 
 const Blog = () => {
   return (
@@ -9,7 +12,7 @@ const Blog = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12 pt-3 pb-3">
-              <h2>Create a new Blog</h2>
+              <h2>Create new Blog</h2>
             </div>
             <div className="col-md-6">
               <CreateBlog />
