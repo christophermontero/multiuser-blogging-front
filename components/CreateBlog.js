eeb9cfef55b650a9fbccf91a28db1db445b13d04
Christopher <cgortizm21@gmail.com>
2022-07-14 13:15:29 -0500

    style(client): show categories and tags in checkbox

--- components/CreateBlog.js
+++ components/CreateBlog.js
@@ -1,5 +1,7 @@
 import { withRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { getCategories } from '../actions/category';
+import { getTags } from '../actions/tag';
 import Editor from './Editor';
 
 const CreateBlog = ({ router }) => {
@@ -11,6 +13,8 @@ const CreateBlog = ({ router }) => {
     }
   };
   const [editorLoaded, setEditorLoaded] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
   const [body, setBody] = useState(blogLocalStore());
   const [values, setValues] = useState({
     error: '',
@@ -29,8 +33,30 @@ const CreateBlog = ({ router }) => {
   }, []);
 
   useEffect(() => {
+    initCategories();
+    initTags();
     setValues({ ...values, formData: new FormData() });
   }, [router]);
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const handleChange = (name) => (e) => {
     const value = name === 'photo' ? e.target.files[0] : e.target.value;
@@ -55,6 +81,38 @@ const CreateBlog = ({ router }) => {
     e.preventDefault();
     console.log('ready to publish');
   };
+
+  const showCategories = categories && (
+    <ul
+      className="list-unstyled"
+      style={{ maxHeight: '200px', overflow: 'scroll' }}
+    >
+      {categories.map((category, i) => {
+        return (
+          <li key={i}>
+            <input className="form-check-input me-2" type="checkbox" />
+            <label className="form-check-label">{category.name}</label>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
+  const showTags = tags && (
+    <ul
+      className="list-unstyled"
+      style={{ maxHeight: '200px', overflow: 'scroll' }}
+    >
+      {tags.map((tag, i) => {
+        return (
+          <li key={i}>
+            <input className="form-check-input me-2" type="checkbox" />
+            <label className="form-check-label">{tag.name}</label>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
   const createBlogForm = () => {
     return (
@@ -84,7 +142,21 @@ const CreateBlog = ({ router }) => {
     );
   };
 
-  return <>{createBlogForm()}</>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8">{createBlogForm()}</div>
+        <div className="col-md-4">
+          <h3>Categories</h3>
+          <hr />
+          {showCategories}
+          <h3>Tags</h3>
+          <hr />
+          {showTags}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(CreateBlog);
