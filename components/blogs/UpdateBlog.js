bb24fc15a971b3c1153a52ca35b6855fd577bd10
Christopher Ortiz <cgortizm21@gmail.com>
2023-05-14 20:06:53 -0500

    feat(ui): Load categories and tags checked

--- components/blogs/UpdateBlog.js
+++ components/blogs/UpdateBlog.js
@@ -1,23 +1,36 @@
 import { withRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { singleBlog } from '../../actions/blog';
+import { getCategories } from '../../actions/category';
+import { getTags } from '../../actions/tag';
 import Editor from '../Editor';
 
 const UpdateBlog = ({ router }) => {
   const [editorLoaded, setEditorLoaded] = useState(false);
   const [body, setBody] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [checkedCat, setCheckedCat] = useState([]);
+  const [checkedTag, setCheckedTag] = useState([]);
   const [values, setValues] = useState({
     error: '',
-    success: '',
     formData: '',
     title: ''
   });
 
-  const { formData, title } = values;
+  const { title, photo } = values;
+
+  useEffect(() => {
+    setEditorLoaded(true);
+    initBlog();
+    initCategories();
+    initTags();
+    setValues({ ...values, formData: new FormData() });
+  }, [editorLoaded, router]);
 
   const handleBody = (e) => {
     setBody(e);
-    formData.set('body', e);
+    values.formData.set('body', e);
   };
 
   const editBlog = (e) => {
@@ -26,7 +39,7 @@ const UpdateBlog = ({ router }) => {
 
   const handleChange = (name) => (e) => {
     const value = name === 'photo' ? e.target.files[0] : e.target.value;
-    formData.append(name, value);
+    values.formData.append(name, value);
     setValues({
       ...values,
       [name]: value,
@@ -71,16 +84,116 @@ const UpdateBlog = ({ router }) => {
         } else {
           setValues({ ...values, title: data.title });
           setBody(data.body);
+          setCategoriesArray(data.categories);
+          setTagsArray(data.tags);
         }
       });
     }
   };
 
-  useEffect(() => {
-    setEditorLoaded(true);
-    setValues({ ...values, formData: new FormData() });
-    initBlog();
-  }, []);
+  const setCategoriesArray = (blogCategories) => {
+    const categories = [];
+    blogCategories.map((category) => {
+      categories.push(category._id);
+    });
+    setCheckedCat(categories);
+  };
+
+  const setTagsArray = (blogTags) => {
+    const tags = [];
+    blogTags.map((tag) => {
+      tags.push(tag._id);
+    });
+    setCheckedTag(tags);
+  };
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
+
+  const handleCatToggle = (category) => () => {
+    const clickedCategory = checkedCat.indexOf(category);
+    const allCategories = [...checkedCat];
+    if (clickedCategory === -1) {
+      allCategories.push(category);
+    } else {
+      allCategories.splice(clickedCategory, 1);
+    }
+    values.formData.append('categories', allCategories);
+    setValues({ ...values, error: '' });
+    setCheckedCat(allCategories);
+  };
+
+  const handleTagToggle = (tag) => () => {
+    const clickedTag = checkedTag.indexOf(tag);
+    const allTags = [...checkedTag];
+    if (clickedTag === -1) {
+      allTags.push(tag);
+    } else {
+      allTags.splice(clickedTag, 1);
+    }
+    values.formData.append('tags', allTags);
+    setValues({ ...values, error: '' });
+    setCheckedTag(allTags);
+  };
+
+  const showCategories = categories && (
+    <ul
+      className="list-unstyled px-1"
+      style={{ maxHeight: '200px', overflow: 'scroll' }}
+    >
+      {categories.map((category, i) => {
+        return (
+          <li key={i}>
+            <input
+              onChange={handleCatToggle(category._id)}
+              checked={checkedCat.indexOf(category._id) !== -1}
+              className="form-check-input me-2"
+              type="checkbox"
+            />
+            <label className="form-check-label">{category.name}</label>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
+  const showTags = tags && (
+    <ul
+      className="list-unstyled px-1"
+      style={{ maxHeight: '200px', overflow: 'scroll' }}
+    >
+      {tags.map((tag, i) => {
+        return (
+          <li key={i}>
+            <input
+              onChange={handleTagToggle(tag._id)}
+              checked={checkedTag.indexOf(tag._id) !== -1}
+              className="form-check-input me-2"
+              type="checkbox"
+            />
+            <label className="form-check-label">{tag.name}</label>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
   return (
     <div className="container-fluid pb-2">
@@ -95,8 +208,35 @@ const UpdateBlog = ({ router }) => {
           <div>
             <div className="form-group pb-2">
               <h5>Featured image</h5>
+              <hr />
+              <div>
+                <label className="btn btn-outline-info me-2">
+                  Upload featured image
+                  <input
+                    onChange={handleChange('photo')}
+                    type="file"
+                    accept="image/*"
+                    hidden
+                  />
+                </label>
+                {photo ? (
+                  <div className="p-1">
+                    <small className="text-muted">{photo.name}</small>
+                  </div>
+                ) : (
+                  <div className="p-1">
+                    <small className="text-muted">Max size: 1 Mb</small>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
+          <h3>Categories</h3>
+          <hr />
+          {showCategories}
+          <h3>Tags</h3>
+          <hr />
+          {showTags}
         </div>
       </div>
     </div>
