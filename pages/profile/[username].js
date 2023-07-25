f8c422a3797094b458258d64fc0de40f3e6f5bcf
Christopher Ortiz <cgortizm21@gmail.com>
2023-07-25 09:48:04 -0500

    feat(ui): Add action for fetch user blogs

---
+++ pages/profile/[username].js
@@ -0,0 +1,23 @@
+import Layout from '../../components/Layout';
+
+const UserPublicProfile = () => {
+  return (
+    <>
+      <Layout>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card">
+                <div className="card-body">
+                  <h5>username</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default UserPublicProfile;
