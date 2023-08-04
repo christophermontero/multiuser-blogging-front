5d178836db510ba3f3f878d70a903edef3312dee
Christopher Ortiz <cgortizm21@gmail.com>
2023-08-04 17:29:18 -0500

    feat(ui): List the blogs in the user profile page

--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -1,15 +1,74 @@
+import moment from 'moment';
+import Head from 'next/head';
+import Link from 'next/link';
+import { userPublicProfile } from '../../actions/user';
 import Layout from '../../components/Layout';
+import { API, APP_NAME } from '../../config';
 
-const UserPublicProfile = () => {
+const UserPublicProfile = ({ user, blogs, query }) => {
+  const head = () => (
+    <Head>
+      <title>
+        {user.name} | {APP_NAME}
+      </title>
+      <meta name="description" content={`Blogs by ${user.username}`} />
+      <link rel="canonical" href={`${API}/profile/${query.username}`} />
+    </Head>
+  );
+
+  const showUserBlogs = () => {
+    return blogs.map((blog, i) => {
+      return (
+        <div className="my-4" key={i}>
+          <Link href={`/blogs/${blog.slug}`}>
+            <a className="lead">{blog.title}</a>
+          </Link>
+        </div>
+      );
+    });
+  };
   return (
     <>
+      {head()}
       <Layout>
-        <div className="container">
+        <div className="container pt-3">
           <div className="row">
             <div className="col-md-12">
               <div className="card">
                 <div className="card-body">
-                  <h5>username</h5>
+                  <h5>{user.name}</h5>
+                  <Link href={`${user.profile}`}>
+                    <a>View Profile</a>
+                  </Link>
+                  <p className="text-muted">
+                    Joined {moment(user.createdAt).fromNow()}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="container pb-5">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title bg-primary py-4 px-4 text-white">
+                    Recent blogs by {user.name}
+                  </h5>
+                  {showUserBlogs()}
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title bg-primary py-4 px-4 text-white">
+                    Recent blogs by {user.name}
+                  </h5>
+                  <br />
+                  <p>Contact form</p>
                 </div>
               </div>
             </div>
@@ -18,6 +77,16 @@ const UserPublicProfile = () => {
       </Layout>
     </>
   );
+};
+
+UserPublicProfile.getInitialProps = ({ query }) => {
+  return userPublicProfile(query.username).then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      return { user: data.user, blogs: data.blogs, query };
+    }
+  });
 };
 
 export default UserPublicProfile;
