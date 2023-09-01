2319e096db222bc315b46a481686fb6ef38906fb
Christopher Ortiz <cgortizm21@gmail.com>
2023-08-31 19:54:32 -0500

    fix(ui): Remove link from index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
 
 const Index = () => {
   return (
     <Layout>
       <h2>Index page</h2>
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
     </Layout>
   );
 };
